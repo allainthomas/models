db3cc8e065e59aee26beb87993fc72ecc7a5e0fc
allainthomas <thomallain@gmail.com>
2021-02-10 09:43:09 +0100

    feat : updated dataset, annot_files, tfrecords and charset

--- custom_generator/src/get_tf_records.py
+++ custom_generator/src/get_tf_records.py
@@ -7,106 +7,11 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
-ANNOTATION_FILE = 'custom_generator/data/annot_file.csv'
-CROP_DIR = 'custom_generator/data/crops'
+ANNOTATION_FILE = 'data/annot_file.csv'
+CROP_DIR = 'data/crops'
 
-MAX_STR_LEN = 16
-null = 133
-
-def get_char_mapping():
-	label_file = 'research/attention_ocr/python/datasets/data/number_plates/charset-labels.txt'
-	with open(label_file, "r", encoding='utf-8') as f:
-		char_mapping = {}
-		rev_char_mapping = {}
-
-		for line in f.readlines():
-
-			m, c = line.split("\n")[0].split("\t")
-			char_mapping[c] = m
-			rev_char_mapping[m] = c
-
-	return char_mapping, rev_char_mapping
-
-def read_image(img_path):
-	return cv2.imread(img_path)
-
-def padding_char_ids(char_ids_unpadded, null_id = null, max_str_len=MAX_STR_LEN):
-	return char_ids_unpadded + [null_id for x in range(max_str_len - len(char_ids_unpadded))]
-
-def get_bytelist_feature(x):
-	return tf.train.Feature(bytes_list = tf.train.BytesList(value=x))
-
-def get_floatlist_feature(x):
-	return tf.train.Feature(float_list = tf.train.FloatList(value=x))
-
-def get_intlist_feature(x):
-	return tf.train.Feature(int64_list = tf.train.Int64List(value=[int(y) for y in x]))
-
-def get_tf_example(img_file, annotation, num_of_views=1):
-
-	with tf.io.gfile.GFile(img_file, 'rb') as fid:
-		encoded_jpg = fid.read()
-	# img = gfile.FastGFile(img_file, 'rb').read()
-
-	img_array = read_image(img_file)
-	char_map, _ = get_char_mapping()
-
-	split_text = [x for x in annotation]
-	char_ids_unpadded = [char_map[x] for x in split_text]
-	char_ids_padded = padding_char_ids(char_ids_unpadded)
-
-
-	# print(char_ids_padded)
-
-	char_ids_unpadded = [int(x) for x in char_ids_unpadded]
-	char_ids_padded = [int(x) for x in char_ids_padded]
-
-	print(get_bytelist_feature([annotation.encode('utf-8')]))
-
-	features = tf.train.Features(feature = {
-	'image/format': get_bytelist_feature([b'png']),
-
-	'image/encoded': get_bytelist_feature([encoded_jpg]),
-
-	'image/class': get_intlist_feature(char_ids_padded),
-	'image/unpadded_class': get_intlist_feature(char_ids_unpadded),
-	# 'image/height': get_intlist_feature([img_array.shape[0]]),
-	'image/width': get_intlist_feature([img_array.shape[1]]),
-	'image/orig_width': get_intlist_feature([img_array.shape[1]/num_of_views]),
-	'image/text': get_bytelist_feature([annotation.encode('utf-8')])
-		}
-	)
-	#print(get_intlist_feature(char_ids_padded))
-	example = tf.train.Example(features=features)
-
-	return example
-
-def get_tf_records():
-	train_file = 'train.tfrecord'
-	test_file = 'test.tfrecord'
-	if os.path.exists(train_file):
-		os.remove(train_file)
-	if os.path.exists(test_file):
-		os.remove(test_file)
-	train_writer = tf.io.TFRecordWriter(train_file)
-	test_writer = tf.io.TFRecordWriter(test_file)
-	annot = pd.read_csv(ANNOTATION_FILE, sep = ';', nrows = 750)
-	files = list(annot['files'].values)
-	random.shuffle(files)
-
-	for i, file in enumerate(files):
-		print('Iteration : ', i ,' writing file:', file)
-		annotation = annot[annot['files'] == file]
-		annotation = str(annotation['text'].values[0])
-		example = get_tf_example(CROP_DIR + '/' + file, annotation)
-		if i < 650:
-			train_writer.write(example.SerializeToString())
-		else:
-			test_writer.write(example.SerializeToString())
-
-
-	train_writer.close()
-	test_writer.close()
+MAX_STR_LEN = 13
+null = 41
 
 
 def _im_feature_to_im(example, key):
@@ -145,6 +50,105 @@ def tf2_read_record():
 		print("Number of images :", no)
 		no+=1
 		cv2.imshow("test", image)
+
+def get_char_mapping():
+	label_file = '../research/attention_ocr/python/datasets/data/number_plates/charset-labels.txt'
+	with open(label_file, "r", encoding='utf-8') as f:
+		char_mapping = {}
+		rev_char_mapping = {}
+
+		for line in f.readlines():
+			# print(line)
+			m, c = line.split("\n")[0].split("\t")
+			# print(m,c)
+			char_mapping[c] = m
+			rev_char_mapping[m] = c
+
+	return char_mapping, rev_char_mapping
+
+def read_image(img_path):
+	return cv2.imread(img_path)
+
+def padding_char_ids(char_ids_unpadded, null_id = null, max_str_len=MAX_STR_LEN):
+	return char_ids_unpadded + [null_id for x in range(max_str_len - len(char_ids_unpadded))]
+
+def get_bytelist_feature(x):
+	return tf.train.Feature(bytes_list = tf.train.BytesList(value=x))
+
+def get_floatlist_feature(x):
+	return tf.train.Feature(float_list = tf.train.FloatList(value=x))
+
+def get_intlist_feature(x):
+	return tf.train.Feature(int64_list = tf.train.Int64List(value=[int(y) for y in x]))
+
+def get_tf_example(img_file, annotation, num_of_views=1):
+
+	with tf.io.gfile.GFile(img_file, 'rb') as fid:
+		encoded_jpg = fid.read()
+	# img = gfile.FastGFile(img_file, 'rb').read()
+
+	img_array = read_image(img_file)
+	char_map, _ = get_char_mapping()
+
+	split_text = [x for x in annotation]
+	char_ids_unpadded = [char_map[x] for x in split_text]
+	char_ids_padded = padding_char_ids(char_ids_unpadded)
+
+	# print(char_ids_padded)
+
+	char_ids_unpadded = [int(x) for x in char_ids_unpadded]
+	char_ids_padded = [int(x) for x in char_ids_padded]
+
+	print(get_bytelist_feature([annotation.encode('utf-8')]))
+	# print("Length : ", len(get_bytelist_feature([annotation.encode('utf-8')]).bytes_list.value[0]))
+
+	features = tf.train.Features(feature = {
+	'image/format': get_bytelist_feature([b'png']),
+
+	'image/encoded': get_bytelist_feature([encoded_jpg]),
+
+	'image/class': get_intlist_feature(char_ids_padded),
+	'image/unpadded_class': get_intlist_feature(char_ids_unpadded),
+	# 'image/height': get_intlist_feature([img_array.shape[0]]),
+	'image/width': get_intlist_feature([img_array.shape[1]]),
+	'image/orig_width': get_intlist_feature([img_array.shape[1]/num_of_views]),
+	'image/text': get_bytelist_feature([annotation.encode('utf-8')])
+		}
+	)
+	#print(get_intlist_feature(char_ids_padded))
+	example = tf.train.Example(features=features)
+
+	return example
+
+def get_tf_records():
+	train_file = 'train.tfrecord'
+	test_file = 'test.tfrecord'
+	if os.path.exists(train_file):
+		os.remove(train_file)
+	# if os.path.exists(test_file):
+	# 	os.remove(test_file)
+	train_writer = tf.io.TFRecordWriter(train_file)
+	test_writer = tf.io.TFRecordWriter(test_file)
+	annot = pd.read_csv(ANNOTATION_FILE, sep = ';', nrows = 433)
+	files = list(annot['files'].values)
+	random.shuffle(files)
+
+	for i, file in enumerate(files):
+		print('Iteration : ', i ,' writing file:', file)
+		annotation = annot[annot['files'] == file]
+		annotation = str(annotation['text'].values[0])
+		example = get_tf_example(CROP_DIR + '/' + file, annotation)
+		if i < 650:
+			train_writer.write(example.SerializeToString())
+		# else:
+		# 	test_writer.write(example.SerializeToString())
+
+
+	train_writer.close()
+	test_writer.close()
+
+
+
 
 if __name__ == '__main__':
 	get_tf_records()
--- research/attention_ocr/python/datasets/number_plates.py
+++ research/attention_ocr/python/datasets/number_plates.py
@@ -2,6 +2,7 @@ import datasets.fsns as fsns
 
 DEFAULT_DATASET_DIR = '/content/models/research/attention_ocr/python/datasets/data/number_plates'
 
+"""
 DEFAULT_CONFIG = {
     'name':
         'number_plates', # you can change the name if you want.
@@ -24,6 +25,38 @@ DEFAULT_CONFIG = {
         16, # TO BE CONFIGURED
     'null_code':
         133,
+    'items_to_descriptions': {
+        'image':
+            'A 3 channel color image.',
+        'label':
+            'Characters codes.',
+        'text':
+            'A unicode string.',
+        'length':
+            'A length of the encoded text.',
+        'num_of_views':
+            'A number of different views stored within the image.'
+    }
+}"""
+
+DEFAULT_CONFIG = {
+    'name':
+        'number_plates', # you can change the name if you want.
+    'splits': {
+        'train': {
+            'size': 432, # change according to your own train-test split
+            'pattern': 'train.tfrecord'
+        }
+    },
+    'charset_filename':
+        'charset-labels.txt',
+    'image_shape': (100,200,3),#(max_width, max_height, 3),
+    'num_of_views':
+        1,
+    'max_sequence_length':
+        13, # TO BE CONFIGURED
+    'null_code':
+        41,
     'items_to_descriptions': {
         'image':
             'A 3 channel color image.',
